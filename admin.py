2a7d4d04d6fe11ab4884bd0e1d7fb3c353c9d048
changwoo <note8335@naver.com>
2020-02-20 22:45:11 +0900

    delete

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from myapp.models import Account, Subject_range, Subject_code, Subject, Evaluation
-
-admin.site.register(Account)
-admin.site.register(Subject_range)
-admin.site.register(Subject_code)
-admin.site.register(Subject)
-admin.site.register(Evaluation)
--- test.py
+++
@@ -1,7 +0,0 @@
-print("master!")
-
-print("kimchangwoo")
-
-print("kimchangwoo again")
-
-# changwoo 브렌치로 푸쉬한 주석
